a7850903ded86597b1799c02f21ee222a8b7c3f8
tdorobantu <tdorobantu95@gmail.com>
2022-02-11 14:59:39 +0200

    Started work on NavOptions

---
+++ components/NavOptions.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>navOptions</Text>
+    </View>
+  )
+}
+
+export default NavOptions
